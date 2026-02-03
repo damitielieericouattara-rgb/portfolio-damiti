07f0f13928d1679a514496be8b220eb1f051bbd3
damitielieericouattara-rgb <damitielieericouattara@gmail.com>
2026-02-03 12:54:16 +0000

    MISE A JOURS OHHH

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin:'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings:['Developpeur Fullstack', 'Etudiant en informatique'],
+    strings:['Developpeur front-end', 'Etudiant en informatique'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
